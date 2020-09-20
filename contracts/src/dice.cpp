0cb749a534bd375122090748e617fc23a7dd4185
georgy <wtf496@gmail.com>
2020-09-20 22:04:45 +0900

    Update game according to new rules - range-dice.

--- contracts/src/dice.cpp
+++ contracts/src/dice.cpp
@@ -25,13 +25,18 @@ void dice::check_bet(uint64_t ses_id) const {
 }
 
 void dice::check_action_params(const std::vector<game_sdk::param_t>& params) const {
-    eosio::check(params.size() == 1, "params amount should be 1");
-    eosio::check(params[0] > 0, "number should be more than 0");
-    eosio::check(params[0] < 100, "number should be less than 100");
+    eosio::check(params.size() == 2, "params amount should be 2");
+    eosio::check(params[0] > 0, "small number should be more than 0");
+    eosio::check(params[0] < 100, "small number should be less than 100");
+
+    eosio::check(params[1] > 1, "big number should be more than 1");
+    eosio::check(params[1] <= 100, "big number should be less or equal than 100");
+
+    eosio::check(params[0] < params[1], "small number should be less than big number");
 }
 
-double dice::get_win_coefficient(dice_number_t num) {
-    return (constant::all_range / (constant::all_range - num)) * (1. - constant::house_edge);
+double dice::get_win_coefficient(dice_number_t num_small, dice_number_t num_big) {
+    return (constant::all_range / (constant::all_range - (num_big - num_small))) * (1. - constant::house_edge);
 }
 
 void dice::on_new_game(uint64_t ses_id) {
@@ -46,15 +51,17 @@ void dice::on_action(uint64_t ses_id, uint16_t type, std::vector<game_sdk::param
     eosio::check(type == constant::roll_action_type, "allowed only roll action with type 0");
     check_action_params(params);
 
-    const dice_number_t number = params[0]; ///< "params[0]" is a player choise
+    const dice_number_t num_small = params[0]; ///< "params[0]" is a player small number choice
+    const dice_number_t num_big = params[1]; ///< "params[1]" is a player big number choice
     rolls.emplace(get_self(), [&](auto& row) {
         row.ses_id = ses_id;
-        row.number = number;
+        row.number_small = num_small;
+        row.number_big = num_big;
     });
 
     // Here we say to platform the amount of maximum possible payout
     // It's platform requirement, game must provide that amount every time when it's changed
-    update_max_win(get_win_payout(ses_id, number));
+    update_max_win(get_win_payout(ses_id, num_small, num_big));
 
     // Request random number
     // After that contract will initiate secure random generation protocol
@@ -62,11 +69,11 @@ void dice::on_action(uint64_t ses_id, uint16_t type, std::vector<game_sdk::param
     require_random();
 }
 
-asset dice::get_win_payout(uint64_t ses_id, dice_number_t number) const {
+asset dice::get_win_payout(uint64_t ses_id, dice_number_t num_small, dice_number_t num_big) const {
     auto win_payout = get_session(ses_id).deposit;
 
     // Assets can't be multiply to double. Use dirty way.
-    win_payout.amount = uint64_t(double(win_payout.amount) * get_win_coefficient(number));
+    win_payout.amount = uint64_t(double(win_payout.amount) * get_win_coefficient(num_small, num_big));
 
     const auto max_payout = asset(*get_param_value(ses_id, constant::max_payout_param_type), core_symbol);
     return win_payout < max_payout ? win_payout : max_payout;
@@ -79,12 +86,13 @@ void dice::on_random(uint64_t ses_id, checksum256 rand) {
 
     auto payout = zero_asset;
 
-    const auto bet_number = rolls.get(ses_id).number;
-    if (bet_number <= actual_number) { // Win
-        payout = get_win_payout(ses_id, bet_number);
+    const auto bet_num_small = rolls.get(ses_id).number_small;
+    const auto bet_num_big = rolls.get(ses_id).number_big;
+    if (bet_num_small <= actual_number && actual_number < bet_num_big) { // Win
+        payout = get_win_payout(ses_id, bet_num_small, bet_num_big);
     }
 
-    // Initiate game finalization and fund transfering to player and casino
+    // Initiate game finalization and fund transferring to player and casino
     finish_game(payout, std::vector<game_sdk::param_t> {actual_number});
 }
 
